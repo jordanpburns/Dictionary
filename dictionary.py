8cb1fc381b09ffd200da864a940160e07d2fb53f
jordanpburns <41701519+jordanpburns@users.noreply.github.com>
2018-08-04 17:49:32 +0200

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -3,8 +3,10 @@ from zlib import decompress
 import os
 import re
 
-# this is some gnarly stuff I found on the internet to unpack/decompress the dictionary.
-# should work for any downloaded mac dicitonary, just enter the desired filename
+""" This is some gnarly stuff I found on the internet to unpack/decompress the dictionary.
+It is from commenter "korakot" on https://gist.github.com/josephg/5e134adf70760ee7e49d
+This should work for any downloaded mac dictionary, just enter the desired filename. Here 
+I've used an English - German dictionary"""
 
 filename = "/System/Library/Assets/com_apple_MobileAsset_DictionaryServices_dictionaryOSX/2a679af89e5bb8b417ff02b0fef81ca39ffad9a5.asset/AssetData/German - English.dictionary/Contents/Resources/Body.data"
 f = open(filename, 'rb')
@@ -22,4 +24,5 @@ with open('dict.xml', 'wb') as fo:
   for s in gen_chunk():
     fo.write(s)
 
+    # this runs a bash script I wrote to extract the useful stuff from the xml
 os.system('bash getWords.bash')

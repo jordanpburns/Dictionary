a9e6d5058247d6f04e8054af896ba217803e4460
jordanpburns <41701519+jordanpburns@users.noreply.github.com>
2018-08-04 18:02:07 +0200

    Update getWords.bash

--- getWords.bash
+++ getWords.bash
@@ -2,12 +2,12 @@
 LANG=C sed -i -e 's/%/percent/g' dict.xml
 
 # now print a word followed by all translations separated by commas
-egrep -o '''"trans">''[^\<]*<|''d:title="(.+?)"''' dict.xml | awk -F '\"|\>|\ \<|\<' '/trans/{printf ","$4}/d:title/{printf "\n"$2}' >> words
+egrep -o '''"trans">''[^\<]*<|''d:title="(.+?)"''' dict.xml | awk -F '\"|\>|\ \<|\<' '/trans/{printf ","$4}/d:title/{printf "\n"$2}' >> wordlist
 
 # clean up file
 
 # delete words with no translations ie. has no commas
-sed -i -e '/,/!d' words
+sed -i -e '/,/!d' wordlist
 
-# delete extra commas for translations that came out as "" for some reason
-sed -i -e 's/,{2,100}/,/g' words
+# delete extra commas for translations that came out as "" for some reason, still doesn't work right though
+sed -i -e 's/,{2,100}/,/g' wordlist
